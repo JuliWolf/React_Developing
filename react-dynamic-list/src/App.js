141124d77b3e6f55f43d724aaec156da3280c695
JuliWolf <qwert0038@yandex.ru>
2020-07-06 16:01:43 +0300

    Updated module styles

--- react-dynamic-list/src/App.js
+++ react-dynamic-list/src/App.js
@@ -1,21 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import styled from 'styled-components';
 import Person from "./Person/Person";
 
-const StyledButton = styled.button`
-    background-color: ${props => props.alt ? 'red': 'green'};
-    color: white;
-    font: inherit;
-    border: 1px solid blue;
-    padding: 8px;
-    cursor: pointer;
-    
-    &:hover {
-        background-color: ${props => props.alt ? 'salmon': 'lightgreen'};
-        color: black;
-    }
-`
 
 class App extends Component {
     state  = {
@@ -87,11 +73,10 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm React App</h1>
                 <p className={classes.join(' ')}>This is really working!</p>
-                <StyledButton
-                    alt={this.state.showPersons}
+                <button className='button'
                     onClick={this.togglePersonsHandler}>
                     Switch Name
-                </StyledButton>
+                </button>
                 {persons}
             </div>
         )
