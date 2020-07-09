9823d9bad7d678abcd8cab6a0bc6b8fb411220a0
JuliWolf <qwert0038@yandex.ru>
2020-07-09 19:11:50 +0300

    Order summary backdrop, buttons

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -82,6 +82,13 @@ class BurgerBuilder extends Component {
     purchaseHandler =  () => {
         this.setState({purchasing: true})
     }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!')
+    }
 
     render(){
         const disableInfo = {
@@ -92,8 +99,15 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                <Modal
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}
+                >
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCancel={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
