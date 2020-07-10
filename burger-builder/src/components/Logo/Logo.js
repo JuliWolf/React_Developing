b7913387358eed1feaab0d05d8dc7d9ae3d6c727
JuliWolf <qwert0038@yandex.ru>
2020-07-10 14:36:18 +0300

    Menu for mobile desktop devices

--- burger-builder/src/components/Logo/Logo.js
+++ burger-builder/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './Logo.css'
 
 import burderLogo from '../../assets/images/burger-logo.png'
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burderLogo} alt="MyBurger"/>
     </div>
 );
