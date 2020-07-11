5ae9edf47132f27cdec54b35a9940db4d1179aa0
JuliWolf <qwert0038@yandex.ru>
2020-07-11 18:49:38 +0300

    Lazy loading with React

--- 6. Routing/src/containers/Blog/Blog.js
+++ 6. Routing/src/containers/Blog/Blog.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 // import axios from '../../axios';
-import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-
+// import NewPost from './NewPost/NewPost';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import './Blog.css';
+
+import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost')
+})
+
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -40,7 +45,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
                 {/*<Route path="/new-post" exact render={() => <h1>Home 2</h1>}/>*/}
                 <Switch>
-                    { this.state.auth ? <Route path="/new-post" component={NewPost}/> : null }
+                    { this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null }
                     <Route path="/posts" component={Posts}/>
 
                     {/* attr "from" in Redirect is allowed only in Switch*/}
