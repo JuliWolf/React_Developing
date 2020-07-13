9f34a64b18f63c186b98d0f230235b935271f3d4
JuliWolf <qwert0038@yandex.ru>
2020-07-13 10:02:51 +0300

    actions in constants

--- 8. Redux/src/containers/Counter/Counter.js
+++ 8. Redux/src/containers/Counter/Counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -34,6 +35,14 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(result => (
+                        <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}> {result.value}</li>
+                    ))}
+
+                </ul>
             </div>
         );
     }
@@ -41,16 +50,19 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-      ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddCounter: () => dispatch({type: 'ADD', val: 10}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15})
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAddCounter: () => dispatch({type: actionTypes.ADD, val: 10}),
+        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 15}),
+        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
 
     }
 }
--- 8. Redux/src/store/reducer.js
+++ 8. Redux/src/store/reducer.js
@@ -1,25 +1,53 @@
+import * as actionTypes from './actions';
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
-
+    // const newState = Object.assign({}, state);
+    const newState = {...state};
     switch(action.type){
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
+            newState.counter = state.counter + 1;
+            return newState
+        case actionTypes.DECREMENT:
             return {
-                counter: state.counter + 1
-            };
-        case 'DECREMENT':
-            return {
+                ...state,
                 counter: state.counter - 1
             };
-        case 'ADD':
+        case actionTypes.ADD:
             return {
+                ...state,
                 counter: state.counter + action.val
             };
-        case 'SUBTRACT':
+        case actionTypes.SUBTRACT:
             return {
+                ...state,
                 counter: state.counter - action.val
+            };
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: new Date(),
+                    value: state.counter
+                })
+            };
+
+        case actionTypes.DELETE_RESULT:
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1);
+            const updatedArray = state.results.filter(
+                (result, index) =>
+                    result.id !== action.resultElId
+            )
+
+            return {
+                ...state,
+                results: updatedArray
             };
     }
     return state;
