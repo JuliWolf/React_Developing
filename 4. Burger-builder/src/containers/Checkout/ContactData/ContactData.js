802b8d94068e022b45f32a7b55bc7a6eaac4d17c
JuliWolf <qwert0038@yandex.ru>
2020-07-17 17:28:01 +0300

    splash checkValidity

--- 4. Burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ 4. Burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import Axios from '../../../axios-orders';
 import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from '../../../store/actions/index'
-import {updateObject} from "../../../shared/utility";
+import {updateObject, checkValidity} from "../../../shared/utility";
 
 import classes from './ContactData.css'
 
@@ -122,39 +122,10 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token)
     }
 
-    checkValidity(value, rules){
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-        return isValid;
-    }
-
     inputChangedHandler = (event, id) => {
         const updatedFormElement =  updateObject(this.state.orderForm[id], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.orderForm[id].validation),
+            valid: checkValidity(event.target.value, this.state.orderForm[id].validation),
             touched: true
         });
         const updatedOrderForm = updateObject(this.state.orderForm, {
