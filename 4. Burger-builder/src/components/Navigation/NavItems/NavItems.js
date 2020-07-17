ab0aa68cf4f13610810d55986ad022113c1272d3
JuliWolf <qwert0038@yandex.ru>
2020-07-17 15:19:27 +0300

    redirect from login page

--- 4. Burger-builder/src/components/Navigation/NavItems/NavItems.js
+++ 4. Burger-builder/src/components/Navigation/NavItems/NavItems.js
@@ -5,7 +5,7 @@ import NavItem from './NavItem/NavItem'
 const navItems = (props) => (
     <ul className={classes.NavItems}>
         <NavItem link="/" exact>Burger Builder</NavItem>
-        <NavItem link="/orders">Orders</NavItem>
+        {props.isAuthenticated ? <NavItem link="/orders">Orders</NavItem> : null}
         {props.isAuthenticated
             ? <NavItem link="/logout">Logout</NavItem>
             : <NavItem link="/auth">Authenticate</NavItem>
