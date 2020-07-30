e972d7301a8c500f94f3b9739751a96eacb4431b
JuliWolf <qwert0038@yandex.ru>
2020-07-30 16:57:00 +0300

    Animations in React

---
+++ 12. Animation/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+    state = {
+        modelIsOpen: false
+    }
+
+    showModal = () => {
+        this.setState({modelIsOpen: true})
+    }
+
+    closeModal = () => {
+        this.setState({modelIsOpen: false})
+    }
+    render() {
+        return (
+            <div className="App">
+                <h1>React Animations</h1>
+                {this.state.modelIsOpen ? <Modal closed={this.closeModal}/> : null}
+                {this.state.modelIsOpen ?  <Backdrop show={this.state.modelIsOpen}/> : null}
+                <button className="Button" onClick={this.showModal}>Open Modal</button>
+                <h3>Animating Lists</h3>
+                <List />
+            </div>
+        );
+    }
+}
+
+export default App;
