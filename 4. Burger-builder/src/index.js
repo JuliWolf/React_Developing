7925ef29a3c05efbcf9d4762dba9556db16a949b
JuliWolf <qwert0038@yandex.ru>
2020-09-29 22:29:54 +0300

    Redux saga

--- 4. Burger-builder/src/index.js
+++ 4. Burger-builder/src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import burderBuilderReducer from './store/reducers/burderBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { logout } from './store/sagas/auth';
 
 import './index.css';
 
@@ -18,12 +20,20 @@ const rootReducer = combineReducers({
     burgerBuilder: burderBuilderReducer,
     order: orderReducer,
     auth: authReducer
-})
+});
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = process.env.NODE_ENV === 'development'
     ?  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  : null || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk, sagaMiddleware)
+    )
+);
+
+sagaMiddleware.run(logout);
 
 const app = (
     <Provider store={store}>
