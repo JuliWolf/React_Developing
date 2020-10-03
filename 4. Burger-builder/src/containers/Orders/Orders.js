444f1e4280323f7521df4dec3e9f2d0be3428c28
JuliWolf <qwert0038@yandex.ru>
2020-10-03 16:22:39 +0300

    convert Orders.js into functional component

--- 4. Burger-builder/src/containers/Orders/Orders.js
+++ 4. Burger-builder/src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux'
 import Axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
@@ -6,24 +6,23 @@ import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner'
 
-class Orders extends Component {
-    componentDidMount() {
-        this.props.onFetchOrders(this.props.token, this.props.userId);
-    }
+const Orders = props => {
 
-    render() {
-        let orders = <Spinner/>;
-        if(!this.props.loading){
-            orders = (<div>
-                {this.props.orders.map(order => (
-                    <Order key={order.id}
-                           ingredients={order.ingredients}
-                           price={+order.price}/>
-                ))}
-            </div>)
-        }
-        return orders
+    useEffect(() => {
+        props.onFetchOrders(props.token, props.userId);
+    }, []);
+
+    let orders = <Spinner/>;
+    if(!props.loading){
+        orders = (<div>
+            {props.orders.map(order => (
+                <Order key={order.id}
+                       ingredients={order.ingredients}
+                       price={+order.price}/>
+            ))}
+        </div>)
     }
+    return orders
 }
 const mapStateToProps = state => {
     return {
