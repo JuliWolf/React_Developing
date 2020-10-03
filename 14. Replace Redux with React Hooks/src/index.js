7abba526f27082c86c8714ce0cc28c427a003303
JuliWolf <qwert0038@yandex.ru>
2020-10-03 22:24:31 +0300

    comments

--- 14. Replace Redux with React Hooks/src/index.js
+++ 14. Replace Redux with React Hooks/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
 import ProductsProvider from './context/products-context';
 
 ReactDOM.render(
@@ -15,3 +14,9 @@ ReactDOM.render(
   </ProductsProvider>,
   document.getElementById('root')
 );
+
+// UseContext используется для тех действий, которые выполняются не частно
+// Так как useContext не имеет возможности определять какой компонент и где обновить
+// При изменении данных, каждый компонент, подписанных на события из useContext будет обновлен
+// Что приведет к уменьшению скорости загрузки
+// Подходит для смены тем/авторизации
